5b687106c5f5427932883b10fb4219acfa617c0f
JFizz13 <jasmine.sutton@live.com>
2026-03-11 00:27:39 -0700

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,4 @@
+# Customers: Represents a user and links to their purchase history so the app can validate real users.
+# PurchaseHistory: Stores a customer’s past transactions and supports checking whether purchases exist.
+# Products: Represents menu items with name, price, category, and popularity rating for browsing/filtering.
+# Transactions: Groups selected products into one purchase and calculates the total cost.
